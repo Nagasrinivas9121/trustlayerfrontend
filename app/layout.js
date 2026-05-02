d479a0ae45fcb6b05857f93388d3ada804747b32
Nagasrinivas9121 <nagasrinivasaraoeevuri@gmail.com>
2026-05-02 20:48:12 +0530

    feat: integrate Google Tag Manager (GTM-N98LZ37G)

--- app/layout.js
+++ app/layout.js
@@ -98,6 +98,27 @@ export default function RootLayout({ children }) {
           }}
         />
         
+        {/* Google Tag Manager (GTM) */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N98LZ37G');
+          `}
+        </Script>
+
+        {/* noscript fallback for GTM */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N98LZ37G"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         {/* Scripts optimized for non-blocking execution */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-51DXDHGGHS"
